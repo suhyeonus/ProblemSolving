f2c2a93ac332942be865a1214cc6eae1859b2da5
SuHyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2024-05-05 14:19:19 +0900

    [Gold V] Title: 토마토, Time: 1604 ms, Memory: 48488 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -11,16 +11,12 @@ for i in range(H):
             if graph[i][j][k] == 1:
                 q.append((i, j, k))
 
-dx = [-1,1,0,0,0,0]
-dy = [0,0,1,-1,0,0]
-dz = [0,0,0,0,1,-1]
+d = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
 def bfs():
     while q:
         x, y, z = q.popleft()
-        for i in range(6):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            nz = z + dz[i]
+        for dx, dy, dz in d:
+            nx, ny, nz = x + dx, y + dy, z + dz
             if 0<=nx<H and 0<=ny<N and 0<=nz<M and graph[nx][ny][nz] == 0:
                 graph[nx][ny][nz] = graph[x][y][z] + 1
                 q.append((nx, ny, nz))
