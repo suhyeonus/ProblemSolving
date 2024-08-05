5f4114be5a0317400679d287399733e67c009a6f
SuHyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2024-08-05 19:24:38 +0930

    [Bronze I] Title: 약수들의 합, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -1,12 +1,5 @@
 while True:
     n = int(input())
     if n == -1: break
-    a = []
-    for i in range(1, n):
-        if n % i == 0: a.append(i)
-    if sum(a) != n: print(f"{n} is NOT perfect.")
-    else:
-        print(f"{n} = ", end="")
-        newa = []
-        for i in a: newa.append(str(i))
-        print(' + '.join(newa))
+    a = [i for i in range(1, n) if n % i == 0]
+    print(f"{n} is NOT perfect.") if sum(a) != n else print((f"{n} = ")+" + ".join(map(str,a)))
