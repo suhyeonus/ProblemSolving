fc7eaaa48dc47e3c93bc5964d7f4f0490c9fb14a
SuHyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2024-09-08 19:57:52 +0930

    [Silver IV] Title: 요세푸스 문제, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,22 +1,14 @@
-# 백준 1158: 요세푸스 문제 / 실버4
-# 풀이 시간:
-# 큐
+import sys 
+input = sys.stdin.readline 
 
-# queue = 1 ~ N번까지의 원.
-# 1 꼭 더해주기
 N, K = map(int, input().split())
-queue = [_ for _ in range(1, N + 1)]
-answer = []
+q = [i for i in range(1, N+1)]
+ans = []
 index = 0
 
-for i in range(N):
-    # 1을 빼주는 이유: 원소가 하나씩 삭제되기 때문에
+while q:
     index += K - 1
-    # 안 빼주면 out of range 발생
-    if index >= len(queue):
-        index %= len(queue)
-    # answer에 해당 원소를 추가해주고, 그대로 pop
-    answer.append(queue.pop(index))
+    if index >= len(q): index %= len(q)
+    ans.append(q.pop(index))
 
-print("<", end="")
-print(', '.join(map(str, answer)) + '>')
+print("<" + ", ".join(map(str,ans)) + '>')
