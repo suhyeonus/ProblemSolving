6abb9cf2440b9d7f3208fb8bb938474499424345
SuHyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2024-10-08 20:50:31 +1030

    [Silver III] Title: 프린터 큐, Time: 72 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -5,15 +5,20 @@ input = sys.stdin.readline
 for _ in range(int(input())):
     N, M = map(int, input().split())
 
-    # order : 차례를 저장, queue: 프린터 큐
+    # now: index, order: 각 index의 초기 순서를 저장
+    now = 1
     order = [0] * N 
+
+    # printq: 입력받은 큐
+    # queue: 입력받은 큐를 초기 순서와 함께 튜플화
     printq = list(map(int, input().split()))
     queue = deque([])
     for i in range(len(printq)): queue.append((printq[i], i))
-    now = 1
     
     while queue:
-        if order[M] != 0: break
+        if order[M]: break
+
+        # x: 프린터 큐의 원소, y: x의 초기 순서
         x, y = queue.popleft()
         if not queue or x >= max(queue, key=lambda x:x[0])[0]:
             order[y] = now 
