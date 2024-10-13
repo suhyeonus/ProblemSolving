59462ee2c970b49c28a86a8306901afd1665b3fe
SuHyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2024-10-13 13:49:29 +1030

    [Gold IV] Title: 문자열 폭발, Time: 764 ms, Memory: 71552 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -7,7 +7,7 @@ stk = []
 
 for i in S:
     stk.append(i)
-    if ''.join(map(str, stk[-len(expl):])) == expl:
+    if ''.join(stk[-len(expl):]) == expl:
         for _ in range(len(expl)): stk.pop()
 
 print(*stk, sep='') if stk else print("FRULA")
