d2a2b252c282c6162c42faf697bdef4030939a4a
SuHyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2024-12-08 17:01:56 +1030

    [Silver V] Title: K-세준수, Time: 1500 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1418. K－세준수/K－세준수.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline 
+
+n = int(input())
+k = int(input())
+ans = 0
+
+for i in range(1, n+1):
+    # i == 1일 때도 조건 성립하므로 추가
+    if i == 1: 
+        ans += 1
+    else:
+        tmp = 2
+        while i > 1:
+            # 소인수분해 가능한 수가 k보다 클 경우 바로 break
+            if tmp > k: break
+            if not i % tmp:
+                while not i % tmp:
+                    i //= tmp 
+            tmp += 1
+        else: ans += 1
+
+print(ans)
