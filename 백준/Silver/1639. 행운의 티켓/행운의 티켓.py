8dd0cf3b561f8dd4871a6f96dfdda2c6f046d9b1
Suhyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2025-02-04 21:52:38 +0900

    [Silver IV] Title: 행운의 티켓, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1639. 행운의 티켓/행운의 티켓.py
+++ 백준/Silver/1639. 행운의 티켓/행운의 티켓.py
@@ -6,8 +6,6 @@ answer = 0
 
 for i in range(len(ticket)):
     for j in range(i+1, len(ticket), 2):
-        # print(ticket[i : j+1])
-        # print(ticket[i : (i+j)//2 + 1], ticket[(i+j)//2 + 1 : j+1])
         if sum(map(int, ticket[i : (i+j)//2 + 1])) == sum(map(int, ticket[(i+j)//2 + 1 : j+1])):
             answer = max(answer, len(ticket[i : j+1]))
 
