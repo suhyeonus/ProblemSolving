e25293bf247c556abbf6b2dae8de2e774d4ea6ab
Suhyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2025-05-06 16:36:22 +0900

    [Gold III] Title: 앱, Time: 136 ms, Memory: 14896 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -8,21 +8,20 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int answer = Integer.MAX_VALUE;
         int[] dp = new int[10001];
-        int[] memory = new int[N];
-        int[] cost = new int[N];
+        int memory = 0, cost = 0;
+        int answer = Integer.MAX_VALUE;
 
         StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
         StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
 
         for(int i = 0; i < N; i++) {
-            memory[i] = Integer.parseInt(st1.nextToken());
-            cost[i] = Integer.parseInt(st2.nextToken());
+            memory = Integer.parseInt(st1.nextToken());
+            cost = Integer.parseInt(st2.nextToken());
             
             for(int j = 10000; j >= 0; j--) {
-                if(i == 0 && j >= cost[i]) dp[j] = memory[i];
-                else if(j >= cost[i]) dp[j] = Math.max(dp[j-cost[i]] + memory[i], dp[j]); 
+                if(i == 0 && j >= cost) dp[j] = memory;
+                else if(j >= cost) dp[j] = Math.max(dp[j-cost] + memory, dp[j]); 
                 
                 if(dp[j] >= M) answer = Math.min(j, answer);
             }
