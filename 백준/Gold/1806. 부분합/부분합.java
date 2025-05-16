21f2ba1b90d43e6eb85da04a7e639d7bdfc55148
Suhyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2025-05-16 13:19:24 +0900

    [Gold IV] Title: 부분합, Time: 252 ms, Memory: 24488 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
 	
@@ -12,18 +13,19 @@ public class Main {
 		
 		st = new StringTokenizer(br.readLine(), " ");
 		long arr[] = new long[n+1];
-		for(int i = 0; i < n; i++) 
+		for(int i = 1; i <= n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] += arr[i-1];
+		}
 		
 		int len = Integer.MAX_VALUE;
-		int start = 0, end = 0;
-		int sum = 0;
+		int start = 1, end = 1;
 		while(end <= n && start <= end) {
-			if (sum < s) {
-				sum += arr[end++];
-			} else if (sum >= s) {
-				len = Math.min(len, end-start);
-				sum -= arr[start++];
+			if (arr[end] - arr[start-1] >= s) {
+				len = Math.min(len, end-start+1);
+				start++;
+			} else {
+				end++;
 			}
 		}
 		
