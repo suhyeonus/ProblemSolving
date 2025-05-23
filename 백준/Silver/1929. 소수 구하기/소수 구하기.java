3f8226274e4e975b743dd70493070c5a25b35536
Suhyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2025-05-23 16:40:24 +0900

    [Silver III] Title: 소수 구하기, Time: 648 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N+1];
+		arr[1] = 1;
+		
+		for(int i = 2; i <= Math.sqrt(N); i++) {
+			if(arr[i] == 1) continue;
+			for(int j = i*i; j <= N; j += i)
+				arr[j] = 1;
+		}
+		
+		for(int i = M; i <= N; i++)
+			if(arr[i] == 0) System.out.println(i);
+	}
+	
+	
+}
