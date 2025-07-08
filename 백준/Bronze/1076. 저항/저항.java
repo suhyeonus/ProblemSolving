dde3d82e35f500c28725e95c905f254cb3392728
Suhyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2025-07-08 22:29:56 +0900

    [Bronze II] Title: 저항, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/1076. 저항/저항.java
+++ 백준/Bronze/1076. 저항/저항.java
@@ -6,22 +6,12 @@ class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		HashMap<String, int[]> h = new HashMap<>();
-		h.put("black" , new int[]{0, 1});
-		h.put("brown" , new int[]{1, 10});
-		h.put("red"   , new int[]{2, 100});
-		h.put("orange", new int[]{3, 1000});
-		h.put("yellow", new int[]{4, 10000});
-		h.put("green" , new int[]{5, 100000});
-		h.put("blue"  , new int[]{6, 1000000});
-		h.put("violet", new int[]{7, 10000000});
-		h.put("grey"  , new int[]{8, 100000000});
-		h.put("white" , new int[]{9, 1000000000});
+		String[] color = {"black", "brown", "red", "orange", "yellow", 
+						  "green", "blue", "violet", "grey", "white"};
 		
-		long answer = 0;
-		answer += h.get(br.readLine())[0] * 10;
-		answer += h.get(br.readLine())[0];
-		System.out.print(answer * h.get(br.readLine())[1]);
+		long answer = Arrays.asList(color).indexOf(br.readLine()) * 10
+					+ Arrays.asList(color).indexOf(br.readLine());
+		System.out.print(answer * (int)Math.pow(10, Arrays.asList(color).indexOf(br.readLine())));
 	}
 }
 
