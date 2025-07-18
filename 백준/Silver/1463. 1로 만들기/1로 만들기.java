e1bcba0b49a80e0aef089fb7d3ef7d527a3fb0f9
Suhyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2025-07-18 23:05:27 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18028 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,20 +1,27 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		int[] dp = new int[N+1];
-		dp[1] = 0;
-		
-		for(int i = 2; i <= N; i++) {
-			dp[i] = dp[i-1] + 1;
-			if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
-			if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
-		}
-		
-		System.out.print(dp[N]);
-	}
+
+    static String s;
+    static List<String> dict;
+    static StringBuilder sb1, sb2, sb3, sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + 1;
+            if(i % 2 == 0) dp[i] = Math.min(dp[i/2]+1, dp[i]);
+            if(i % 3 == 0) dp[i] = Math.min(dp[i/3]+1, dp[i]);
+        }
+
+        System.out.print(dp[n]);
+    }
+
 }
